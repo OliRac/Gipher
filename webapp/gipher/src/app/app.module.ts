28b679201d706477bef8406dadbc80ca50ce14c1
Racette <olivier.racette@cgi.com>
2021-08-19 15:20:41 -0400

    generated needed angular components

--- webapp/gipher/src/app/app.module.ts
+++ webapp/gipher/src/app/app.module.ts
@@ -11,12 +11,24 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchComponent } from './search/search.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { RecommendedComponent } from './recommended/recommended.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserRegistrationComponent
+    UserRegistrationComponent,
+    UserLoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    SearchComponent,
+    FavoritesComponent,
+    RecommendedComponent
   ],
   imports: [
     BrowserModule,
