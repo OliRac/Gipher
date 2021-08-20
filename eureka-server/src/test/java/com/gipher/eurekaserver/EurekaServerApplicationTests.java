2d8012b21b739551f5480b9f5e9ada8b6039b369
leo.silao <leo.silao@cgi.com>
2021-08-20 09:51:32 -0400

    add eureka-server, gateway (user-service) and made favorite service as a eureka-client

---
+++ eureka-server/src/test/java/com/gipher/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gipher.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ favoriteservice/src/main/java/com/tackroute/favoriteservice/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.tackroute.favoriteservice.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+public class TestController {
+    @GetMapping("/hello")
+    public String firstPage() {
+        return "Hello World";
+    }
+}
