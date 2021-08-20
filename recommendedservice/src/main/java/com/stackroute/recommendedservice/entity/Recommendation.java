a40fd46e2773e500f29f59da5b235b299904dfd6
leo.silao <leo.silao@cgi.com>
2021-08-20 11:53:10 -0400

    boilerplate for recommended-service

---
+++ recommendedservice/src/main/java/com/stackroute/recommendedservice/entity/Recommendation.java
@@ -0,0 +1,11 @@
+package com.stackroute.recommendedservice.entity;
+
+import java.util.HashMap;
+
+public class Recommendation {
+    private HashMap<String, Integer> tags;
+    private int userID;
+    private static int threshold = 2;
+
+
+}
