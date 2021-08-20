08e1667323b35f475787fd135225bb4895292928
leo.silao <leo.silao@cgi.com>
2021-08-20 16:44:54 -0400

    add spring cloud gateway and separate the auth service(user-service) from the gateway

---
+++ gateway/src/main/java/com/stackroute/gateway/config/GatewayConfig.java
@@ -0,0 +1,25 @@
+package com.stackroute.gateway.config;
+
+import com.stackroute.gateway.filter.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+
+@Configuration
+public class GatewayConfig {
+
+    @Autowired
+    private JwtAuthenticationFilter filter;
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes().route(  r -> r.path("/auth/**")
+                                        .filters(f -> f.filter(filter)).uri("http://localhost:9999"))
+                                .route( r -> r.path("/favorite-service/**")
+                                        .filters(f -> f.filter(filter)).uri("http://localhost:9093"))
+                                .build();
+    }
+
+}
--- userservice/src/main/java/com/stackroute/userservice/UserServiceApplication.java
+++ userservice/src/main/java/com/stackroute/userservice/UserServiceApplication.java
@@ -5,11 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.io.File;
 
@@ -18,9 +19,7 @@ import java.io.File;
 @EnableJpaRepositories(basePackages="com.stackroute.userservice.repository")
 @EntityScan(basePackages="com.stackroute.userservice.entity")
 @EnableAutoConfiguration
-@EnableZuulProxy
 @EnableEurekaClient
-@EnableFeignClients
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
@@ -28,5 +27,11 @@ public class UserServiceApplication {
 
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 
 }
