4e2712a274b5416353e6b37ac535327e6b2bac08
leo.silao <leo.silao@cgi.com>
2021-08-30 12:57:02 -0400

    fix merge conflicts

--- searchservice/src/main/java/com/stackroute/searchservice/SearchserviceApplication.java
+++ searchservice/src/main/java/com/stackroute/searchservice/SearchserviceApplication.java
@@ -1,19 +1,9 @@
 package com.stackroute.searchservice;
 
-import com.stackroute.searchservice.model.SearchEngine;
-import org.springframework.amqp.core.AmqpAdmin;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 @EnableEurekaClient
