b94dafd5743243a95b94fc01ee037c3921c5e085
Racette <olivier.racette@cgi.com>
2021-08-31 10:37:04 -0400

    final configuration for docker. added powershell script to build images. modified mysql user creation to accept user from 172.%.%.%.

---
+++ build_images.ps1
@@ -0,0 +1,26 @@
+﻿cd $PSScriptRoot/eureka-server
+mvn clean package
+docker build -t springboot-eureka-server:1.0 . 
+
+cd $PSScriptRoot/favoriteservice
+mvn clean package
+docker build -t springboot-favoriteservice:1.0 . 
+
+cd $PSScriptRoot/gateway
+mvn clean package
+docker build -t springboot-spring-cloud-gateway:1.0 . 
+
+cd $PSScriptRoot/recommendedservice
+mvn clean package
+docker build -t springboot-recommendedservice:1.0 .
+
+cd $PSScriptRoot/searchservice
+mvn clean package
+docker build -t springboot-searchservice:1.0 .
+
+cd $PSScriptRoot/userservice
+mvn clean package
+docker build -t springboot-userservice:1.0 .
+
+Write-Host "Success! Press any key to continue ..."
+$host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
