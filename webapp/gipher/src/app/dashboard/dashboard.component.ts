8053fad21f7a5e8afa379e381dfe8002e67342b2
suzan.sharifulnasaby <suzan.sharifulnasaby@cgi.com>
2021-08-31 15:04:32 -0400

    added profile picture to menu

--- webapp/gipher/src/app/dashboard/dashboard.component.ts
+++ webapp/gipher/src/app/dashboard/dashboard.component.ts
@@ -1,35 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { Gif } from '../models/Gif';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { SearchService } from '../services/search.service';
 import { TrendingService } from '../services/trending.service';
 import { Router } from '@angular/router';
 import { parseTenorResponseForGifs } from '../util/tenorResponse.parser';
 
-
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
-
   gifData: Gif[];
-  display : string="recommended";
+  display: string = 'recommended';
   searchValue: string;
   errorMsg: string;
   userId: number;
-  message : string ;
+  message: string;
 
-  username :string ='Julie';
+  username: string = 'Julie';
+  imageUrl: string;
 
   form: FormGroup;
 
   constructor(
     private searchService: SearchService,
     private formBuilder: FormBuilder,
-    private trendingService:TrendingService,
-    private  router : Router
+    private trendingService: TrendingService,
+    private router: Router
   ) {
     this.form = this.formBuilder.group({
       searchTerm: new FormControl('', [Validators.required]),
@@ -37,44 +41,43 @@ export class DashboardComponent implements OnInit {
   }
 
   ngOnInit(): void {
-  //this migth be changed to this.recommendation.getGifs()
-  this.gifData = parseTenorResponseForGifs(this.trendingService.getGifs());
-  this.message ="Here are some recommended gifs for you"
+    //this migth be changed to this.recommendation.getGifs()
+    this.gifData = parseTenorResponseForGifs(this.trendingService.getGifs());
+    this.message = 'Here are some recommended gifs for you';
   }
 
   get searchTerm() {
     return this.form.get('searchTerm');
   }
 
-  changeMessage(content : string){
-    if(content=="search"){
-      this.message="Here is the result of your search"
+  changeMessage(content: string) {
+    if (content == 'search') {
+      this.message = 'Here is the result of your search';
     }
 
-    if(content=="recommended"){
-        this.message="Here are some recommended gifs for you"
-      }
-    if(content=="favorites"){
-            this.message="Here are all your favorites gifs"
-          }
-
+    if (content == 'recommended') {
+      this.message = 'Here are some recommended gifs for you';
+    }
+    if (content == 'favorites') {
+      this.message = 'Here are all your favorites gifs';
+    }
   }
 
-  gridDisplay( content : string) : boolean {
-      this.changeMessage(content)
-     if(this.display==content){
-        return true;}
-     else{
-        return false;}
+  gridDisplay(content: string): boolean {
+    this.changeMessage(content);
+    if (this.display == content) {
+      return true;
+    } else {
+      return false;
+    }
   }
-
 
   onClickLogOut(): void {
-    if(sessionStorage.getItem("user") != null) {
-      sessionStorage.removeItem("user");
-      this.router.navigate(["/landing"]);
+    if (sessionStorage.getItem('user') != null) {
+      sessionStorage.removeItem('user');
+      this.router.navigate(['/landing']);
     }
-   }
+  }
 
   // onSubmit(): void {
   //   if (this.form.valid) {
@@ -90,5 +93,4 @@ export class DashboardComponent implements OnInit {
   //     );
   //   }
   // }
-
 }
