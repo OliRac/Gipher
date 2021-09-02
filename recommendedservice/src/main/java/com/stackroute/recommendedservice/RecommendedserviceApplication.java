254dbf54d24320cb37fb393ac9c1fb6262bf7aa1
leo.silao <leo.silao@cgi.com>
2021-09-02 10:39:27 -0400

    add eureka for recommendation service

--- recommendedservice/src/main/java/com/stackroute/recommendedservice/RecommendedserviceApplication.java
+++ recommendedservice/src/main/java/com/stackroute/recommendedservice/RecommendedserviceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableRabbit
 @EnableCaching
 @SpringBootApplication
+@EnableEurekaClient
 public class RecommendedserviceApplication {
 
 	public static void main(String[] args) {
