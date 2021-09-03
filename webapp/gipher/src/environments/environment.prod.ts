57d8ebf8a3342c2931c33dce4c493773aff8a4b2
leo.silao <leo.silao@cgi.com>
2021-09-02 20:00:50 -0400

    dockerize frontend

--- webapp/gipher/src/environments/environment.prod.ts
+++ webapp/gipher/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  SEARCH_SERVICE_URL: "http://localhost:9090/api/v1/search-service",
+  USER_SERVICE_URL: "http://localhost:9090",
+  FAVORITE_SERVICE_URL: "http://localhost:9090/api/v1/favorite-service",
+  RECOMM_SERVICE_URL: "http://localhost:9090/api/v1/recommendation-service"
 };
